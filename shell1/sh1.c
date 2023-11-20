8cb4aed755335c9da07845e5e28ca3b01a9cfb39
maryhaidei <comfys21208@gmail.com>
2023-11-20 22:25:14 +0300

    changes

--- shell1/sh1.c
+++ shell1/sh1.c
@@ -2,104 +2,95 @@
 #include <stdlib.h>
 #include <string.h> 
 
-#define m 20 
+#define m 20
 
-long long op=0, start=0; 
-
-char *input_str(char *c){
-    long long i=0; 
-    char* str= malloc(m);
-    if(!str) return NULL; 
-    while(*c && *c!= EOF && *c!='\n' && !strchr(" &|;.><()",*c)){ 
-        if(*c=='"'){
-            *c=getchar(); 
-            while(*c!='"'){ 
-                if(*c=='\n'){
-                    free(str);
-                    printf("You entered the wrong number of quotes!\n"); 
-                    return NULL; 
-                }  
-                str[i++]=*c;
-                if (i%m==0) str=realloc(str,(i+m));
-                *c=getchar(); 
-            }   
-        *c=getchar();
-        continue; 
-        }
-        str[i++]=*c;
-        if (i%m==0) str=realloc(str,(i+m));
-        *c=getchar(); 
-    }
-        if(strlen(str)==0){
-            if(strchr("&|;.><()", *c)){
-                str[0]=*c; 
-                str[1]='\0'; 
-                if(*c==')') op--; 
-                if( *c=='(') {
-                    op++;
-                    //start=i; 
+char** input(char** str){ 
+    char  c; 
+    long long i=0, j=0, e=0; 
+    while((c=getchar())!=EOF){
+        str[i]=malloc(m); 
+        j=0; 
+        while(c!='\n'){
+            if(c==EOF) break; 
+             if(c==' ' && j){
+                if (i%m==0) str=realloc(str,(i+m)*sizeof(char*));
+                str[i][j++]='\0'; 
+                break; 
+             }
+            if(c==' '){
+                c=getchar();
+                continue;  
+            } 
+            if(strchr("&|><",c)){
+                if(j){
+                    str[i][j]='\0'; 
+                    printf("%s\n", str[i++]); 
+                    if (i%m==0) str=realloc(str,(i+m)*sizeof(char*));
+                    str[i]=malloc(m); 
                 }
-                i++;  
-            }else {
-                free(str);
-                str=input_str(c);
-            }
-        }else{ 
-            if(i%m==0) str=realloc(str, i+m); 
-            str[i]='\0'; 
-        }
-    return str;
-}
-
-void input(char** str){ 
-    char c; 
-    long long i=0; 
-     while((c=getchar())!=EOF){ 
-        if(!op) start=i;
-        if((op!=0) && (c=='\n')){
-            printf("You entered wrong amount of brackets!\n"); 
-            if( op>0){
-                for(int j=i-1; j>=start; j++) free(str[j]); 
-                i=start; 
-                op=0;
-            }
-             c=getchar(); 
-        }
-        if(c==' ' || c=='\n') continue;
-        if(strchr("&|><", c) && str[i-1][0]==c && str[i-1][1]=='\0'){ 
-            str[i-1][1]=c; 
-            str[i-1][2]='\0'; 
-            continue;
-        }
-        str[i]=input_str(&c); 
-        i++; 
-        if(i%m==0) str=realloc(str, sizeof(char*)*(m+i)); 
-        if( str[i-1]==NULL || (c=='\n' && op)){
-            printf("You entered wrong amount of brackets or quotes!\n"); 
-            if(op>0)for(int j=i-1; j>=start; j++) free(str[j]); 
-            i=start; 
-            op=0; 
-            continue;
-        }
-        if(c=='\n') start=0; 
-        if(strchr("&|;.><()", c)){
-            if(str[i-1][0]!=c){
-                str[i]=malloc(3);
-                str[1][0]=c;  
-                str[i][1]='\0'; 
-                i++;  
-                if (i%m==0) str=realloc(str,(i+m)*sizeof(char *));
-                if(c==')') op--; 
-                if( c=='(') {
-                    op++;
-                    start=i; 
+                str[i][0]=c; 
+                if((c=getchar())==str[i][0]){
+                    str[i][1]=c; 
+                    str[i][2]='\0'; 
+                    //printf("%s\n", str[i]); 
+                    j=2;
+                    break; 
+                }else{ 
+                    str[i][1]='\0'; 
+                    j=1;
+                    if(c=='\n') break;
+                    printf("%s\n", str[i++]); 
+                    if (i%m==0) str=realloc(str,(i+m)*sizeof(char*));
+                    str[i]=malloc(m); 
+                    j=0;
+                    continue;  
                 }
+            }else if(strchr(";()",c)){
+                if(j>0){
+                    //str[i][j]='\0'; 
+                    printf("%s\n", str[i]); 
+                    i++; 
+                    if (i%m==0) str=realloc(str,(i+m)*sizeof(char*));
+                    str[i]=malloc(m);
+                }
+                str[i][0]=c; 
+                str[i][1]='\0';
+                j=1;  
+                break; 
             }
+            if(c=='"'){
+                if((c=getchar())=='"'){
+                    c=getchar(); 
+                    continue; 
+                } 
+                while(c!='"'){
+                    str[i][j++]=c; 
+                    if(j%m==0) str[i]=realloc(str[i], j+m); 
+                    str[i][j]='\0'; 
+                    c=getchar(); 
+                }
+                c=getchar(); 
+                continue; 
+            }
+            str[i][j++]=c;
+            if (j%m==0) str[i]=realloc(str[i],(j+m)*sizeof(char));
+            str[i][j]='\0'; 
+            c=getchar(); 
         }
+        if(c=='\n' && !j) {
+            free(str[i]);
+            continue; 
+        }   
+        if(j && str[i][j-1]!='\0') str[i][j]='\0'; 
+        printf("%s\n", str[i]); 
+        i++;
+       if (i%m==0) str=realloc(str,(i+m)*sizeof(char*));
+        str[i]=NULL;    
+        if(c==EOF) break;   
     }
-    str[i]=NULL;     
+    str[i]=NULL;
+    return str; 
 }
-
 void del(char** str){
     long i=0; 
     while(str[i]) {
@@ -109,19 +100,9 @@ void del(char** str){
     free(str);
 }
 
-void print(char** str){
-    long long i=0;
-    while(str[i]){
-        printf("%s\n", str[i]);
-        i++;
-    }
-}
-
-int main(){
-    char** str =malloc(sizeof(char*)*m); 
-    if(!str) return 0; 
-    input(str); 
-    print(str); 
-    del(str);
+int main(){ 
+    char** str=malloc(m*(sizeof(char*))); 
+    str=input(str); 
+    del(str); 
     return 0; 
 }
